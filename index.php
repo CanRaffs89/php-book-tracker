5dbfd8c9c326f48d9b5fcc189a0497c70d407054
Can <24660563+CanRaffs89@users.noreply.github.com>
2021-01-04 21:40:22 +0000

    Connected to database

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+    // Connect to database
+    $connect = mysqli_connect('localhost', 'can', 'Rainbow57', 'book_tracker');
+
+    // Check connection
+    if(!$connect){
+        echo 'Connection error: '.mysqli_connect_error();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <?php include('templates/header.php'); ?>
